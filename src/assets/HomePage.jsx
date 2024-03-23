a9bdcc95d06b652d2f9cb16bc1f5907053200b21
Mariana <marianabatalhadosloios@gmail.com>
2024-03-23 22:23:31 +0000

    homepage, topbar + css

--- src/assets/HomePage.jsx
+++ src/assets/HomePage.jsx
@@ -1,7 +1,33 @@
-import React from 'react'
+import React from "react";
+import titleImg from "./images/lavanda.png";
+import cacaletop from "./images/cacaleimg.jpg";
 
 export default function HomePage() {
   return (
-    <div>HomePage</div>
-  )
+    <div className="homepage-container">
+
+      <div className="title-container">
+        <img className="lavanda-img" src={titleImg} alt="" />
+        <p>Catarina & Alexandre</p>
+
+        <p>7 de Setembro de 2024</p>
+      </div>
+
+      <div className="announcement-container">
+        Comunicação
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur
+          aliquam quo animi! Quaerat error provident soluta delectus quia harum
+          dolor velit, omnis ex veniam! Iste harum non consectetur minima
+          inventore. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Officiis cum ab placeat, accusantium inventore tempora debitis amet
+          sequi recusandae sed cupiditate eos velit odio eaque nobis ut, quia
+          cumque quasi. Lorem, ipsum dolor sit amet consectetur adipisicing
+          elit. Earum quia, provident a fugiat eius asperiores veniam tempora
+          aliquam repudiandae quas repellat, maxime voluptates itaque corrupti
+          placeat voluptatibus temporibus omnis ad?
+        </p>
+      </div>
+    </div>
+  );
 }
---
+++ src/assets/TopBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function TopBar() {
+  return (
+    <div className="buttons-container">
+      <button className="top-buttons">A Nossa História</button>
+      <button className="top-buttons">O Grande Dia</button>
+      <button className="top-buttons">Guia</button>
+    </div>
+  );
+}
