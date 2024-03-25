fa91f71602a83ac22146597d4b9d061e4432f6cf
Mariana <marianabatalhadosloios@gmail.com>
2024-03-25 23:07:26 +0000

    25/03

--- src/Components/TopBar.jsx
+++ src/Components/TopBar.jsx
@@ -1,17 +1,23 @@
 import React from "react";
 import StoryPage from "../Pages/StoryPage";
 import { useNavigate } from "react-router-dom";
+import titleImg from "../assets/images/lavanda.png";
+import alianca from "../assets/images/aliança.png";
 
 export default function TopBar() {
   const nav = useNavigate();
   return (
-    <div className="buttons-container">
-      <button className="top-buttons" onClick={() => nav("/historia")}>
-        A Nossa História
-      </button>
-      <button className="top-buttons" onClick={() => nav("/dia")}>
-        O Grande Dia
-      </button>
+    <div className="top-container">
+      <div className="logo-container">
+      </div>
+      <div className="button-container">
+        <button className="top-buttons" onClick={() => nav("/historia")}>
+          A Nossa História
+        </button>
+        <button className="top-buttons" onClick={() => nav("/dia")}>
+          O Grande Dia
+        </button>
+      </div>
     </div>
   );
 }
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,32 +1,39 @@
 import React from "react";
-import titleImg from "../assets/images/lavanda.png";
+import raminhos from "../assets/images/raminhos.png";
 
 export default function HomePage() {
   return (
     <div className="homepage-container">
       <div className="title-container">
-        <img className="lavanda-img" src={titleImg} alt="" />
-        <p>Catarina & Alexandre</p>
-
-        <p>7 de Setembro de 2024</p>
+        <img src={raminhos} alt="" style={{ height: "100px" }} />
+        Catarina & Alexandre
+        <br />
+        <div className="data-container">7 de setembro de 2024</div>
       </div>
-
       <div className="announcement-container">
-        Comunicação
         <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur
-          aliquam quo animi! Quaerat error provident soluta delectus quia harum
-          dolor velit, omnis ex veniam! Iste harum non consectetur minima
-          inventore. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          Officiis cum ab placeat, accusantium inventore tempora debitis amet
-          sequi recusandae sed cupiditate eos velit odio eaque nobis ut, quia
-          cumque quasi. Lorem, ipsum dolor sit amet consectetur adipisicing
-          elit. Earum quia, provident a fugiat eius asperiores veniam tempora
-          aliquam repudiandae quas repellat, maxime voluptates itaque corrupti
-          placeat voluptatibus temporibus omnis ad?
+          Temos vindo a escrever uma história linda, que começou quando e onde
+          menos esperávamos. Queremos continuar esta jornada e dar o próximo
+          passo, para sempre enquanto amigos e namorados. Por isso, após 10 anos
+          de namoro e 2 anos de noivado, chegou o momento de ‘dar o nó’!
+          Teríamos o maior prazer em celebrar este nosso novo início com cada um
+          de vós, família e amigos, que, de perto ou de longe, nos acompanham
+          nesta jornada. <span> לחיים!</span>
         </p>
       </div>
-      
+      <button className="rsvp-button">Confirmar Presença</button>
+      <div className="honeymoon-container">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Atque ea sit
+        expedita tenetur soluta nulla, distinctio, deserunt aut dicta corrupti
+        libero nesciunt accusamus enim! Sed reiciendis dolore reprehenderit
+        nesciunt minus. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Vitae magni rerum in nesciunt, velit labore! Illum dicta ab quis,
+        aliquid temporibus sed consequatur dolor perferendis commodi facere
+        delectus velit dignissimos. Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Similique sapiente assumenda neque nam, in, voluptate
+        commodi asperiores libero architecto sequi possimus dicta porro est
+        atque quidem eligendi illum? Consequatur, excepturi!
+      </div>
     </div>
   );
 }
