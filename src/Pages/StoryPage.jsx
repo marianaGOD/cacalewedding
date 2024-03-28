954e65f977c8eea6dbc22a4689f8a8aa3aeb8fe4
Duarte Godinho <duarte.r.godinho@gmail.com>
2024-03-28 10:56:07 +0000

    Swipper & Counter com dias

--- src/Pages/StoryPage.jsx
+++ src/Pages/StoryPage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination } from "swiper";
+import { Navigation, Pagination } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
