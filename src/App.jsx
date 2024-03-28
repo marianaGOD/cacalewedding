d32f67de771132da768ba30e91beb4835f03b42c
Duarte Godinho <duarte.r.godinho@gmail.com>
2024-03-28 16:53:41 +0000

    Fit content under TopBar

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,29 @@ import TopBar from "./Components/TopBar";
 import Form from "./Components/Form";
 import StoryPage from "./Pages/StoryPage";
 import BidDay from "./Pages/BigDay";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [topBarHeight, setTopBarHeight] = useState(0);
+
+  useEffect(() => {
+    // Calculamos a altura da TopBar para puxar as outras coisas para baixo
+    const topBarElement = document.getElementById("mainNavigation");
+    if (topBarElement) {
+      setTopBarHeight(topBarElement.offsetHeight);
+    }
+  }, []);
   return (
     <>
       <TopBar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/form" element={<Form />} />
-        <Route path="/historia" element={<StoryPage />} />
-        <Route path="/dia" element={<BidDay />} />
-      </Routes>
+      <div style={{ paddingTop: `${topBarHeight}px` }}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/form" element={<Form />} />
+          <Route path="/historia" element={<StoryPage />} />
+          <Route path="/dia" element={<BidDay />} />
+        </Routes>
+      </div>
     </>
   );
 }
